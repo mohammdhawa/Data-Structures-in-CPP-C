44aa20751331e0d9abcd9ca24f9df94d6393ab6f
Mohammad Hawa <66217824+mohammdhawa@users.noreply.github.com>
2021-10-18 22:25:44 +0300

    Create Deleting_from_Doubly_Linked_list.cpp

---
+++ Linked_List/CPP-language/Deleting_from_Doubly_Linked_list.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+
+struct Node {
+	Node* prev;
+	int data;
+	Node* next;
+};
+
+class douplyLinkedList {
+	Node* first;
+public:
+	douplyLinkedList(int A[], int n) {
+		first = new Node;
+		Node* t, * last;
+
+		first->data = A[0];
+		first->next = first->prev = nullptr;
+		last = first;
+
+		for (int i = 1; i < n; i++) {
+			t = new Node;
+			t->data = A[i];
+			t->next = last->next;
+			t->prev = last;
+			last->next = t;
+			last = t;
+		}
+	}
+	int length() {
+		int c = 0; 
+		Node* p = first;
+		while (p) {
+			p = p->next;
+			c++;
+		}
+
+		return c;
+	}
+	void display() {
+		Node* p = first;
+
+		while (p) {
+			std::cout << p->data << ' ';
+			p = p->next;
+		}
+
+		std::cout << std::endl;
+	}
+	
+	void deleteFirst() {
+		Node* p = first;
+		first = first->next;
+		int x = p->data;
+		delete p;
+		if (first)
+			first->prev = nullptr;
+
+		std::cout << x << " has been deleted" << std::endl;
+	}
+	void deleteFrom(int pos) {
+		if (pos<1 || pos>length()) {
+			std::cout << "Error: you can not delete from empty list" << std::endl;
+			return;
+		}
+
+		Node* p = first;
+		if (pos == 1) {
+			first = first->next;
+			int x = p->data;
+			delete p;
+			if (first)
+				first->prev = nullptr;
+			std::cout << x << " has been deleted" << std::endl;
+		}
+		else {
+			for (int i = 0; i < pos - 1; i++)
+				p = p->next;
+
+			int x = p->data;
+			p->prev->next = p->next;
+			if (p->next)
+				p->next->prev = p->prev;
+			delete p;
+
+			std::cout << x << " has been deleted" << std::endl;
+		}
+	}
+	void deleteEnd() {
+		if (first == nullptr) {
+			std::cout << "you can not delete from empty list" << std::endl;
+			return;
+		}
+		Node* p = first;
+		while (p->next)
+			p = p->next;
+
+		int x = p->data;
+		p->prev->next = nullptr;
+		delete p;
+
+		std::cout << x << " has been deleted" << std::endl;
+	}
+};
+
+int main() {
+	int A[] = { 11, 22, 33, 44, 55 };
+	int n = sizeof(A) / sizeof(A[0]);
+
+	douplyLinkedList dll(A, n);
+
+	dll.display();
+
+	dll.deleteFirst();
+	dll.display();
+
+	dll.deleteFrom(3);
+	dll.display();
+
+	dll.deleteEnd();
+	dll.display(); 
+	
+	return 0;
+
+}
+
