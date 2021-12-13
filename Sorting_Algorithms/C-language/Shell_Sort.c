41ecce5d243228d2323937d2d9d83a2cbee252e3
Mohammad Hawa <66217824+mohammdhawa@users.noreply.github.com>
2021-12-13 15:41:26 +0300

    Create Shell_Sort.c

---
+++ Sorting_Algorithms/C-language/Shell_Sort.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void ShellSort(int A[], int n)
+{
+    for(int gap = n/2; gap>=1; gap/=2)
+    {
+        for(int j = gap; j<n; j++)
+        {
+            int temp = A[j];
+            int i = j-gap;
+            while(i>=0&&A[i]>temp)
+            {
+                A[i+gap] = A[i];
+                i = i-gap;
+            }
+            A[i+gap] = temp;
+        }
+    }
+}
+
+int main()
+{
+
+	int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	for (int i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+
+	//QuickSort(arr, 0, n-1);
+
+	//iterative_mergeSort(arr, n);
+
+	//mergeSort(arr, 0, n-1);
+
+	//countSort(arr, n);
+
+	//BucketSort(arr, n);
+
+	//RadixSort(arr, n);
+
+	ShellSort(arr, n);
+
+	for (int i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+
+
+	return 0;
+}
