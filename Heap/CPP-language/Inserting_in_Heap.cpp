5b3ac7b32eafd596907176d5691873ce1e8be63a
Mohammad Hawa <66217824+mohammdhawa@users.noreply.github.com>
2021-12-24 17:13:19 +0300

    Create Inserting_in_Heap.cpp

---
+++ Heap/CPP-language/Inserting_in_Heap.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+
+// or using stl
+void insert_stl(std::vector<int>& vec, int key) {
+    auto i = vec.size();
+    vec.emplace_back(key);
+
+    while (i > 0 && key > vec[i % 2 == 0 ? (i / 2) - 1 : i / 2]) {
+        vec[i] = vec[i % 2 == 0 ? (i / 2) - 1 : i / 2];
+        i = i % 2 == 0 ? (i / 2) - 1 : i / 2;
+    }
+    vec[i] = key;
+}
+
+template <class T>
+void print(T& ve, int n) {
+    std::cout << "Max heap: [";
+    for (int i = 0; i < n; i++) {
+        std::cout << ve[i] << ' ';
+        if (i < n - 1)
+            std::cout << ", ";
+    }
+    std::cout << "]" << std::endl;
+}
+
+int main() {
+
+    int a[] = { 45, 35, 15, 30, 10, 12, 6, 5, 20, 50 };
+    Insert(a, 9);
+    print(a, sizeof(a) / sizeof(a[0]));
+    std::cout << std::endl;
+
+
+    return 0;
+}
